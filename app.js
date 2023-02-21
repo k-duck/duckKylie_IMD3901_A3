be4d14f85d48b8a16710d10d4c5a0851a559dc69
Kylie <kyliemduck@gmail.com>
2023-02-20 19:30:10 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { Socket } = require('dgram');
+const express   = require('express');
+const app       = express();
+const http      = require('http');
+const server    = require('http').createServer(app);
+const io        = require('socket.io')(server);
+
+const LISTEN_PORT   = 8080;
+
+server.listen(LISTEN_PORT);
+app.use(express.static(__dirname + '/public'));
+console.log("Listening on port: " + LISTEN_PORT);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id + " is connected");
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + " disconnected");
+    });
+});
