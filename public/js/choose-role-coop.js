107765df71f94eaf04ccad0c382ae32c83425bd6
Kylie <kyliemduck@gmail.com>
2023-02-21 19:20:03 -0500

    Got physics and player movement working, at the cost of using an older version of a-frame

---
+++ public/js/choose-role-coop.js
@@ -0,0 +1,11 @@
+function pick3D(){
+    console.log("3D!!");
+    document.getElementById('player3D').style.display = "block";
+    document.getElementById('start-ui').style.display = "none";
+}
+
+function pick2D(){
+    console.log("2D!!");
+    document.getElementById('player2D').style.display = "block";
+    document.getElementById('start-ui').style.display = "none";
+}
